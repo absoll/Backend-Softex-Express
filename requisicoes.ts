42c9bc64fe2fe2a1ed9addc78aad9ab88f5883bf
absoll <uzumakijunior@gmail.com>
2023-11-28 19:47:26 +0000

    Atualizado com as requisições de POST e PUT

--- requisicoes.ts
+++ requisicoes.ts
@@ -24,12 +24,65 @@ app.get('/livros/:id', (req: Request, res: Response) =>{
     res.send(livro);
 });
 
+
 app.get('/teste', (req: Request, res: Response) => {
     res.send(`teste`);
 })
+
+
+app.post('/post', (req:Request, res:Response) => {
+  
+    //Recebe tudo q vinher e coloca em obj. Não importa o q venha
+    //const obj = req.body
+    //console.log(obj)
+    //console.log(obj.titulo)
+  
+    //Recebe o que vinher e coloca na variavel correspondente. Isso no formato JSON.
+    const {titulo} = req.body
+    console.log(titulo); //Se por acabo nao vinher um nome do dicionario "titulo", esse valor fica vazio(nao existe)
+  
+    if(!titulo)
+      return res.status(404).json({msg: "error"});
+  
+    res.send(`Recebido. `);
+  });
+
+  app.put('/livro', (req: Request, res: Response) => {  
+    const {titulo, autor} = req.body
+  
+    res.json({msg: "Atualizado PUT"})
+  })
+  
+  //Para valores dinamicos vindo da URL
+  app.put('/livro/:id', (req: Request, res: Response) => {  
+    const {titulo, autor} = req.body
+    const id = req.params.id
+    console.log(id)
+  
+    res.json({msg: "Atualizado PUT dinamico"})
+  })
+
+  //Para MAIS DE UM valor dinamico vindo da URL
+  app.put('/livro/:id/:teste', (req: Request, res: Response) => {  
+    const {titulo, autor} = req.body
+    const id = req.params.id
+    const teste = req.params.teste
+    console.log(`ID: ${id} | Teste: ${teste} | Titulo: ${titulo} | Autor: ${autor}`)
+  
+    res.json({msg: "Atualizado PUT mais de um dinamico"})
+  })
+
 
 const porta: number = 3000
 
 app.listen(porta, () => {
     console.log(`Servidor porta: http://localhost:${porta}`)
 })
+
+//IMPORTANTE:
+//https://expressjs.com/pt-br/guide/routing.html
+
+//Caso queira criar caminhos nao definidos, como por exemplo com letras reptidas e tal... Usa os casos de *(qualquer coisa), ?(possuindo essa letra ou nao) e + (qualquer quantidade repetida da letra anterior)
+//ab?cd => acd , abcd
+//ab*cd => abcd, abxcd, abRABDOMcd, ab123cd
+//ab+cd => abcd, abbcd, abbbcd
